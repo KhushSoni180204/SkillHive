0214762a2cf6b61be26ffa8a2c25d03d68b6436a
Khush <khushsoniconnect@gmail.com>
2026-01-27 17:55:01 +0530

    updated gemini_client.py

--- backend/ai_learning/services/gemini_client.py
+++ backend/ai_learning/services/gemini_client.py
@@ -1,11 +1,11 @@
 from google import genai
 from django.conf import settings
 
-client = genai.Client(api_key=settings.GEMINI_API_KEY)
 
 class GeminiService:
     @classmethod
     def generate(cls, prompt: str) -> str:
+        client = genai.Client(api_key=settings.GEMINI_API_KEY)
         response = client.models.generate_content(
             model="gemini-flash-latest",
             contents=prompt,

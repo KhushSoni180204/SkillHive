dd42bbec9509b4fa20b7787fe5fb6a37e04e12f8
Khush <khushsoniconnect@gmail.com>
2026-01-27 18:15:13 +0530

    Updated settings_test

--- backend/skillhive/settings_test.py
+++ backend/skillhive/settings_test.py
@@ -1,10 +1,13 @@
 from .settings import *
 
+# Force in-memory cache for tests
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "test-cache",
     }
 }
 
+# Optional but recommended
 CELERY_TASK_ALWAYS_EAGER = True
 CELERY_TASK_EAGER_PROPAGATES = True
